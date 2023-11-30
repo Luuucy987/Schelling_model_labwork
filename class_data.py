377997359311b3b5b9d83d3d2e3eeac48130ee1d
Lucy <steadily.arthur@gmail.com>
2023-11-30 20:37:56 +0800

    数据结构定义实现 0. 根据权值生成随机标签向量 1. 初始化标签 2. 初始化快乐值 3. 边界补0函数 4. 返回搬家点【快乐值不达到预期】 5. 计算单个点的快乐值 剩余: 1. 处理迭代搬家点 2. 可视化 3. 文件处理

--- class_data.py
+++ class_data.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-N = 50  # 矩阵大小
+N = 5  # 矩阵大小
 empty_label = 0  # 点阵状态为空的标签
 error = 0  # 错误返回值
-label_n = 2  # n个标签，这里初始化设置为2
-label_weight = {0.5, 0.3, 0.2}  # n个标签的权值列表【int】类型列表，第0个表示 标签为空的权值
+label_n = 3  # n个标签，这里初始化设置为3
+label_list = [0, 1, 2]
+label_weight = [0.5, 0.3, 0.2]  # n个标签的权值列表【int】类型列表，第0个表示 标签为空的权值
 
 import copy
 
@@ -46,76 +47,133 @@ class point:
 
 
 class Matrix:
-    def __init__(self):
-        self.matrix = np.array([point(1), point(-1)])  # 生成两个点信息到matrix中【示例一维】
-    # 需实现
-    # 1. n*n二维矩阵初始化->通过标签向量初始化 所有点
+    def __init__(self, _laberrandom, _N: int):
+        """
+        :param _laberrandom: 已初始化好的标签数组队列
+        """
+        self.matrix = get_two_dimensional_matrix_class(_N, _N, label_random=_laberrandom)  # 生成n个点信息到matrix中【二维 N*N】
+        self.cow = _N
+        self.col = _N
 
 
 # 实现随机生成N*N大小的一维向量，向量包含所有点的标签信息
-def f(x: int, y: int, _label_n: int, _weight: int):
+def random_label(x: int, y: int, _label_list: list, _weight: list):
     """
+    :param _label_list: 标签的列表
     :param x: 二维矩阵的行数
     :param y: 二维矩阵的列
-    :param _label_n:n种标签
     :param _weight:n种标签的n+1个权值列表【包含空。所以是n+1】
-    :return: 返回大小为size的一维标签列表
+    :return: 返回大小为size的一维标签数组
     """
     size = x * y
+    random_res = np.random.choice(_label_list, size=size, p=_weight)
+    return random_res
 
 
-# x = np.arange(N)
-# y = np.arange(N)
-# X, Y = np.meshgrid(x, y)
-# status = ["C1", "C2", 'white']  # 网格状态，生成三类格子，white为空白网格
-# prob = [0.4, 0.4, 0.2]  # 三类格子的生成比例
-#
-#
-# def init_z():
-#     """初始化网格
-#     :return: 返回创建的网格
-#     """
-#     Z = np.random.choice(a=status, size=(N ** 2), p=prob)
-#     Z.shape = (N, N)
-#     return Z
-#
-#
-# def get_cell_happiness(Z, row, col):
-#     """获取每个单元格的满意程度阈值
-#     :Z: N*N np.array
-#     :row: int行, col:int列
-#     :return: happiness:int
-#     """
-#     if not Z.shape == (N, N):
-#         Z.shape = (N, N)
-#     if Z[row, col] == "white":
-#         return np.NaN
-#     same, count = 0, 0  # same为相同个数，count为有人的个数
-#     left = 0 if col == 0 else col - 1
-#     right = Z.shape[1] if col == Z.shape[1] - 1 else col + 2
-#     top = 0 if row == 0 else row - 1
-#     bottom = Z.shape[0] if row == Z.shape[0] - 1 else row + 2
-#
-#     for i in range(top, bottom):
-#         for j in range(left, right):
-#             if (i, j) == (row, col) or Z[i, j] == "white":
-#                 continue
-#             elif Z[i, j] == Z[row, col]:
-#                 same += 1
-#                 count += 1
-#             else:
-#                 count += 1
-#     if not count == 0:
-#         happiness = same / count
-#     else:
-#         happiness = 0
-#     return happiness
-#
+def get_two_dimensional_matrix_class(row: int, col: int, label_random):
+    """
+    :param row: n行
+    :param col: n列
+    :param label_random:已经随机过的标签数组，数组大小应该是row * col
+    :return: 返回构造好的np.array 二维对象矩阵,错误则为0
+    """
+    if (len(label_random) != row * col):
+        print("row/col not adapt label_random list")
+        return error
+
+    for j in range(col):
+        for i in range(row):
+            temp = j * col + i
+            temp_c = point(label_random[temp])
+            print(temp_c.label, end=" ")
+        print("")
+    res = np.array([[point(label_random[j * col + i]) for i in range(row)]
+                    for j in range(col)]
+                   )
+    if (res.shape == (row, col)):  # 确保矩阵输出大小和需求一致
+        print("\n matrix size is :", res.shape)
+        return res
+    else:
+        return error
+
+
+def calculate_happy_rate(input_M: Matrix, row: int, col: int, un_happy_list: list, happy_rate_endurance: float):
+    """
+    :param happy_rate_endurance:最低忍耐度，小于这个忍耐度会被认为有搬家倾向
+    :param input_M: Matrix类
+    :param row: 行
+    :param col: 列
+    :param un_happy_list 不开心的点集合，初始传入可以为空
+    :return input_M, 输出新的矩阵类，其实就是改变了他的开心值
+            un_happy_list 输出不开心点集
+    """
+    input_M.matrix = Completion_zero(input_M)  # 将矩阵0填充
+
+    for _col in range(col):
+        for _row in range(row):
+            temp_col = _col + 1
+            temp_row = _row + 1
+            temp_ha_r = calculate_8_happy_rate(input_M.matrix, temp_col, temp_row)
+            if temp_ha_r < happy_rate_endurance and input_M.matrix[temp_col][temp_row].label != 0:
+                un_happy_list.append((temp_col - 1, temp_row - 1))
+            input_M.matrix[temp_col][temp_row].happy_rate = temp_ha_r
+    print(un_happy_list)
+    input_M.matrix = input_M.matrix[1:-1, 1:-1]
+    print(input_M.matrix.shape)
+    return input_M, un_happy_list
+
+
+def calculate_8_happy_rate(matrix_input, x: int, y: int):
+    """
+    :param x: 对应点的坐标位置
+    :param y:
+    :param matrix_input: 类型同 Matrix.matrix,np.array 点阵的合集【已补零】
+    :return:计算单个点的快乐率
+    """
+
+    label = matrix_input[x][y].label
+    if label == empty_label:
+        print(f"matrix_input[{x}][{y}] happy rate is :{0}")
+        return 0.0
+    count_p = 0.0
+    for row in range(3):
+        for col in range(3):
+            x_temp = x - 1
+            y_temp = y - 1
+            x_temp += col
+            y_temp += row
+            if matrix_input[x_temp][y_temp].label == label:
+                count_p += 1.0
+    count_p -= 1
+    # print(count_p)
+    happy_rate = count_p / 8.0
+    matrix_input[x][y].happy_rate = happy_rate
+    print(f"matrix_input[{x}][{y}] happy rate is :{happy_rate}")
+    return happy_rate
+
+
+def Completion_zero(input_M: Matrix):
+    """
+    :param input_M: 输入的矩阵对象
+    :return: 将矩阵对象边缘0填充
+    """
+    zero_fill = point(0)
+    add_M = np.pad(input_M.matrix, 1, constant_values=zero_fill)
+    return add_M
+
 
 # 注重代码规范
 if __name__ == "__main__":
-    test_point = point(1)
-    print(f"test point lebal is {test_point.label}, happy_rate is {test_point.happy_rate}")
-    matrix_t = Matrix()  # 构造一个Matrix类
-    matrix_t0 = matrix_t.matrix[1]  # 提取第0个对象【可以自行修改理解】
-    print(f"matrix[1] is {matrix_t0.label}")  # 打印第0个对象的标签值
+    # point 类测试
+    # test_point = point(1)
+    # print(f"test point lebal is {test_point.label}, happy_rate is {test_point.happy_rate}")
+
+    label_r = random_label(N, N, label_list, label_weight)
+    # print(label_r[0:25])
+    # t_np = get_two_dimensional_matrix_class(5, 5, label_r)
+    # temp = t_np[0][1].label
+    # print(f"temp is {temp}")
+    test_list = list()
+    matrix_t = Matrix(label_r, N)
+    # calculate_8_happy_rate(matrix_t.matrix, 1, 3) #测试单个点的快乐值
+    matrix_res, test_list = calculate_happy_rate(matrix_t, N, N, test_list, 0.375)
--- visualization.py
+++ visualization.py
@@ -2,5 +2,5 @@
 import class_data
 
 
-def test_fuction(test: class_data.civi):
+def test_fuction(test: class_data.Matrix):
     return 0
